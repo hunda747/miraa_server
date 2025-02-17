0d720224a6b867ef7d86993fff311e41eb03f2d4
hunda747 <hundaolnk@gmail.com>
2025-02-17 17:54:51 +0300

    fixed issue

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const roleController = require('../controllers/RoleController.js');
+const roleController = require('../controllers/roleController.js');
 
 // Get all roles
 router.get('/', roleController.getAllRoles);
